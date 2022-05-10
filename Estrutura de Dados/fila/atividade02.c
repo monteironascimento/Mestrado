bbabbb992fcc325e184dbf0cccbe9d2094a0c626
Rodrigo Monteiro do Nascimento <rodrigo.monteiro.nascimento@outlook.com>
2022-05-09 21:48:50 -0300

    atualizacoa

--- Estrutura de Dados/fila/atividade02.c
+++ Estrutura de Dados/fila/atividade02.c
@@ -2,12 +2,47 @@
 #include <string.h>
 
 #include "fila.h"
+#include "pilha.h"
+
+void inverter_fila(Fila* f, Pilha* p){
+
+    TipoElemento elemento;
+    int tamf1 = fila_tamanho(f);
+
+    for (int i = 0; i < tamf1; i++){
+        elemento = 0;
+        fila_remover(f, &elemento);
+        if(elemento > 0){
+            pilha_empilhar(p, elemento);
+        }
+    }
+
+
+    pilha_imprimir(p);
+   
+
+    printf("\n");
+
+}
 
 int main(){
 
     printf("\n");
-    
-    printf("OLA\n");
+
+    printf("OLA MUNDO");
+
+      /**************************************
+    * Teste insercao e primeiro
+    **************************************/
+    Fila* f = fila_criar();
+    fila_inserir(f, 10);
+    fila_inserir(f, 20);
+    fila_inserir(f, 30);
+
+    Pilha* p = pilha_criar();
+
+    inverter_fila(f, p);
+    fila_imprimir(f);
 
     printf("\n");
     return 0;
